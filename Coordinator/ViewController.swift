bf5e0de0b6c58f67fa358115948027720f04964e
Chris Moore <chris@knowndecimal.com>
2020-04-08 19:36:12 -0400

    Initial Commit

---
+++ Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinator
+//
+//  Created by Chris Moore on 4/8/20.
+//  Copyright © 2020 Known Decimal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
